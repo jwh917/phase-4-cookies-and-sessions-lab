3fc330e2f9d7cf91309d97a081a09ba86e06642f
Julien Hnery <jwh_917@yahoo.com>
2022-11-25 22:04:20 -0500

    commit tree f195e5a73128224441c3d574a1c6cbcb7d47af94

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  # rescue_from ActiveRecord::NotAuthorized, with: :max_views
+  # Rails doesn't seem to have an exception to :unauthorized
 
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
@@ -7,14 +9,26 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json: article
+    else 
+      max_views
+    end
+    
   end
 
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+  end
+
+  def max_views
+    render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
   end
 
 end
